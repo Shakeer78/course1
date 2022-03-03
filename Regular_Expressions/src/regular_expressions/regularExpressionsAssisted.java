c4e60808062a3a602e87cedd81cfbf0d9e35a5f6
Shakeer78 <shakeersms786@gmail.com>
2022-03-03 15:43:23 +0530

    Course1

---
+++ Regular_Expressions/src/regular_expressions/regularExpressionsAssisted.java
@@ -0,0 +1,18 @@
+package regular_expressions;
+
+import java.util.regex.*;
+public class regularExpressionsAssisted {
+	public static void main(String[] args) {
+
+		String pattern = "[a-z]+";
+		String check = "Regular Expressions";
+		Pattern p = Pattern.compile(pattern);
+		Matcher c = p.matcher(check);
+		
+		while (c.find())
+	      	System.out.println( check.substring( c.start(), c.end() ) );
+		}
+	}
+
+	
+	
